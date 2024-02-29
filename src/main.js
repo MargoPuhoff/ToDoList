e518957a3f1afa4995bdfc81e0a3f5cfbbe5d1aa
Margo Puhoff <5334936-MargoNiemand@users.noreply.replit.com>
2024-02-29 23:35:25 +0000

    Подключила Ant-design

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import components from '@/components/UI'
+import { DatePicker } from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 
 const app = createApp(App)
 
@@ -8,4 +10,6 @@ components.forEach(component => {
   app.component(component.name, component)
 });
 
-app.mount('#app')
+app
+  .use(DatePicker)
+  .mount('#app')
